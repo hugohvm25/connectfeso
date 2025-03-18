b82d77df185026a7ef78383c27505d90bdcb149a
Hugo <hgmello25@gmail.com>
2025-03-18 20:36:17 -0300

    Atualização da home com maps

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -169,6 +169,8 @@
 // }
 
 
+
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart'; // Importa SpeedDial
@@ -181,21 +183,59 @@ class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  _HomeScreenState createState() => _HomeScreenState();
+  // _HomeScreenState createState() => _HomeScreenState();
+  State<HomeScreen> createState() => _HomeScreenState();
+
 }
 
 class _HomeScreenState extends State<HomeScreen> {
   final AuthService _authService = AuthService();
-  GoogleMapController? _mapController;
-  bool _isMapLoaded = false;
+  late GoogleMapController _mapController; // controlador de mapa
+  Set<Marker> markers = {}; // criar 1 ou mais marcadores no mapa
+  double lat = -22.433915;
+  double long = -42.979152;
 
-  final LatLng _location = LatLng(-22.433798, -42.979064);
+  // Coordenadas dos locais
+  final List<LatLng> locations = [
+    LatLng(-22.433915, -42.979152), // Campus Sede
+    LatLng(-22.393867, -42.959523), // Campus Quinta do Paraíso
+    LatLng(-22.440599, -42.978091), // Centro Cultural Feso Pró-Arte
+  ];
+
+  final List<String> locationNames = [
+    "Campus Sede",
+    "Campus Quinta do Paraíso",
+    "Centro Cultural Feso Pró-Arte",
+  ];
+
+  // CÓDIGO ANTIGO
+
+  // void _onMapCreated(GoogleMapController controller) {
+  //   setState(() {
+  //     _mapController = controller;
+  //     _isMapLoaded = true;
+  //   });
+  // }
 
   void _onMapCreated(GoogleMapController controller) {
-    setState(() {
-      _mapController = controller;
-      _isMapLoaded = true;
-    });
+    _mapController = controller;
+    // Criar os marcadores fixos
+    for (int i = 0; i < locations.length; i++) {
+      final Marker marker = Marker(
+        markerId: MarkerId('marker_$i'),
+        position: locations[i],
+        infoWindow: InfoWindow(
+          title: locationNames[i],
+        ),
+      );
+      setState(() {
+        markers.add(marker);
+      });
+    }
+  }
+
+  void _moveToLocation(LatLng position) {
+    _mapController.animateCamera(CameraUpdate.newLatLng(position));
   }
 
   @override
@@ -222,39 +262,73 @@ class _HomeScreenState extends State<HomeScreen> {
         ],
       ),
 
+
+      // CÓDIGO ANTIGO
+
+      // body: Column(
+      //   children: [
+      //     Container(
+      //       height: 300,
+      //       child: _isMapLoaded
+      //           ? GoogleMap(
+      //         onMapCreated: _onMapCreated,
+      //         initialCameraPosition: CameraPosition(
+      //           target: _location,
+      //           zoom: 15,
+      //         ),
+      //         markers: {
+      //           Marker(
+      //             markerId: MarkerId("local"),
+      //             position: _location,
+      //             infoWindow: InfoWindow(title: "Local Desejado"),
+      //           ),
+      //         },
+      //       )
+      //           : Center(child: CircularProgressIndicator()),
+      //     ),
+      //     SizedBox(height: 20),
+      //     Padding(
+      //       padding: EdgeInsets.all(16),
+      //       child: Column(
+      //         children: [
+      //           Text(
+      //             "Explore este local no mapa e veja em realidade aumentada!",
+      //             style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+      //             textAlign: TextAlign.center,
+      //           ),
+      //           SizedBox(height: 10),
+      //         ],
+      //       ),
+      //     ),
+      //   ],
+      // ),
+
       body: Column(
         children: [
+          // Mapa ocupa metade da tela
           Container(
-            height: 300,
-            child: _isMapLoaded
-                ? GoogleMap(
+            height: MediaQuery.of(context).size.height / 2,
+            child: GoogleMap(
               onMapCreated: _onMapCreated,
               initialCameraPosition: CameraPosition(
-                target: _location,
-                zoom: 15,
+                target: LatLng(lat, long),
+                zoom: 17.5,
               ),
-              markers: {
-                Marker(
-                  markerId: MarkerId("local"),
-                  position: _location,
-                  infoWindow: InfoWindow(title: "Local Desejado"),
-                ),
-              },
-            )
-                : Center(child: CircularProgressIndicator()),
+              markers: markers,
+            ),
           ),
-          SizedBox(height: 20),
-          Padding(
-            padding: EdgeInsets.all(16),
-            child: Column(
-              children: [
-                Text(
-                  "Explore este local no mapa e veja em realidade aumentada!",
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: 10),
-              ],
+          // Lista de botões com os nomes dos locais
+          Expanded(
+            child: ListView.builder(
+              itemCount: locationNames.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(locationNames[index]),
+                  onTap: () {
+                    _moveToLocation(locations[index]);
+                  },
+                );
+              },
             ),
           ),
         ],
@@ -268,15 +342,18 @@ class _HomeScreenState extends State<HomeScreen> {
   // Método para criar o menu flutuante com SpeedDial
   Widget _buildSpeedDial() {
     return SpeedDial(
-      animatedIcon: AnimatedIcons.menu_close, // Ícone animado de abrir/fechar
-      backgroundColor: Color(0xFF006B64), // Cor do botão principal
+      animatedIcon: AnimatedIcons.menu_close,
+      animatedIconTheme: IconThemeData(color: Colors.white, size: 22),
+      backgroundColor: Color(0xFF006B64),
+      foregroundColor: Colors.white,
+      closeManually: false, // Garante que o botão fecha sozinho
       children: [
-        // Botão para Realidade Aumentada
         SpeedDialChild(
           child: Icon(Icons.view_in_ar, color: Colors.white),
           backgroundColor: Color(0xFF006B64),
           label: "Realidade Aumentada",
-          labelStyle: TextStyle(fontSize: 14),
+          labelStyle: TextStyle(fontSize: 14, color: Colors.white),
+          labelBackgroundColor: Color(0xFF006B64),
           onTap: () {
             Navigator.push(
               context,
@@ -284,45 +361,12 @@ class _HomeScreenState extends State<HomeScreen> {
             );
           },
         ),
-
-        // Botão para Chatbot de IA
-        SpeedDialChild(
-          child: Icon(Icons.chat, color: Colors.white),
-          backgroundColor: Color(0xFF006B64),
-          label: "Chatbot de IA",
-          labelStyle: TextStyle(fontSize: 14),
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => ChatScreen()),
-            );
-          },
-        ),
       ],
     );
+
+
   }
 }
-
-
-
-        // @@@@@ MODELO DE BOTÃO PARA O MENU
-
-        // Visibility(
-        //   visible: false, // Pode ser alterado caso precise ocultar
-        //   child: FloatingActionButton(
-        //     heroTag: "btn_ar",
-        //     backgroundColor: Color(0xFF006B64),
-        //     onPressed: () {
-        //       Navigator.push(
-        //         context,
-        //         MaterialPageRoute(builder: (context) => DirecionamentoDeTela()), //Adicionar também no import
-        //       );
-        //     },
-        //     child: Icon(Icons.view_in_ar, color: Colors.white),
-        //   ),
-        // ),
-        // SizedBox(height: 10),
-
 
 
 
